cf3deb93ce18212e49bb334ce7883ad8139c9fcd
Tanguy <35039373+tanguymartinez@users.noreply.github.com>
2018-06-27 13:44:12 +0200

    Modify player script

--- GodotProject/Scenes/Player.gd
+++ GodotProject/Scenes/Player.gd
@@ -3,19 +3,47 @@ extends KinematicBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-export var speed = 10
+export var speedX = 10
+export var speedY = 2
+var startX = 0
+var startY = 0
+var jumping = false
 var playing = false
+var applied_force = Vector2(0,0)
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
+
+func _process(delta):
+	pass
+
+func parabola(x, height, width):
+	return -sqrt(x+width)+height
+
+func jump():
+	if not jumping:
+		jumping = true
+		startX = position.x
+		startY = position.y
+
+
 func _unhandled_input(event):
-    if event is InputEventKey:
-        if event.pressed and event.scancode == KEY_ENTER:
-            playing = !playing
+	if event is InputEventKey and event.pressed:
+		match event.scancode:
+			KEY_ENTER:
+				playing = !playing
+			KEY_SPACE:
+				jump()
 	
 func _physics_process(delta):
 	if playing:
-		move_and_collide(Vector2(speed,0))
+		if jumping:
+			applied_force = Vector2(speedX, speedY*(parabola(position.x+speedX-startX, 200, 200)-parabola(position.x-startX, 200, 200)))
+		else:
+			applied_force = Vector2(speedX, 0)
+		move_and_collide(applied_force)
+		
+			
--- GodotProject/Scenes/World.gd
+++ GodotProject/Scenes/World.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export(PackedScene) var tiles
 export(PackedScene) var scene
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
