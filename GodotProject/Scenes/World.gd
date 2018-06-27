e2cd2d14f3eb032e448edf2222277d5ddb877748
Tanguy <35039373+tanguymartinez@users.noreply.github.com>
2018-06-27 11:56:03 +0200

    Fix world script

--- GodotProject/Scenes/World.gd
+++ GodotProject/Scenes/World.gd
@@ -9,7 +9,7 @@ export(PackedScene) var scene
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	generateGround(5)
+	generateGround(3)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -20,8 +20,9 @@ func generateGround(length):
 	var platform = tiles.instance().get_node("./Ground/platform")
 	print(platform)
 	var position = Vector2()
-	for i in range(0,length):
-		position.x = i*platform.get_scale().x*25
+	$"Ground".add_child(platform)
+	for i in range(length):
+		position.x = i*platform.get_scale().x*50
 		position.y = get_viewport_rect().size.y-25
 		platform = platform.duplicate()
 		platform.position = position
