cd46c20c7982315b3526f0a840a8c22acc8f2be7
Tanguy <35039373+tanguymartinez@users.noreply.github.com>
2018-06-27 12:28:47 +0200

    Fix player script

--- GodotProject/Scenes/Player.gd
+++ GodotProject/Scenes/Player.gd
@@ -11,11 +11,10 @@ func _ready():
 	# Initialization here
 	pass
 
-func _process(delta):
-	# Called every frame. Delta is time since last frame.
-	# Update game logic here.
-	if Input.is_action_pressed("ui_accept"):
-		playing = !playing
+func _unhandled_input(event):
+    if event is InputEventKey:
+        if event.pressed and event.scancode == KEY_ENTER:
+            playing = !playing
 	
 func _physics_process(delta):
 	if playing:
